ded90aa1b6904cf2ab76c180e2a3624d1ec1693d
Amir Hesham <amirhesham65@gmail.com>
2020-09-19 11:31:00 +0200

    Add navigation bar

---
+++ src/components/AppNavbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+
+function AppNavbar() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="#home">20TechJobs</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#link">Github</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default AppNavbar;
