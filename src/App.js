910748f30598613eac7e16521091fa5684048188
Amir Hesham <amirhesham65@gmail.com>
2020-09-19 11:19:24 +0200

    Add React Bootstrap support

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Jumbotron, Button, Container } from "react-bootstrap";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
+      <Container>
+        <header>
+          <Jumbotron>
+            <h1>Welcome to 20TechJobs</h1>
+            <p>
+              A web app that searches for the latest 20 jobs related to a
+              certain keyword fetched from different sources/APIs powered by{" "}
+              <a href="https://github.com/amirhesham65/tech-jobs-api">
+                TechJobs API
+              </a>
+            </p>
+            <Button variant="primary">Learn more</Button>
+          </Jumbotron>
+        </header>
+      </Container>
     </div>
   );
 }
